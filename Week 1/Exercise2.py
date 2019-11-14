5788d4c347afb110c7f3d9ce9d42d79078ccdf9b
Joel Kurian <jkurian94@gmail.com>
2019-11-14 15:41:36 -0500

    exo1 & 2

---
+++ Week 1/Exercise2.py
@@ -0,0 +1,5 @@
+profit=25
+sales=float(input("Total Sales : "))
+total=profit*sales
+print(total)
+print(type(profit))
--- Week 1/exo1.py
+++ Week 1/exo1.py
@@ -1,3 +1,8 @@
-b=float(input("give me a number : "))
-c=b*2
-print(c)
+# b=float(input("give me a number : "))
+# c=b*2
+# print(c)
+
+
+a=int(float(input("give me a number :")))
+b=a+10
+print(b)
